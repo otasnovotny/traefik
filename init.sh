40e0a8e0ba5e73d6ade2cae1b968f95b52a4ac09
Ota Novotný <otas.novotny@gmail.com>
2024-09-18 03:19:13 -0400

    Readme

--- init.sh
+++ init.sh
@@ -1,7 +1,15 @@
 git clone git@github.com:otasnovotny/traefik.git
 cd traefik
+
+# make acme.json
+mkdir data
+cd data
 touch acme.json
 sudo chmod 600 acme.json
+
+# create network
+docker network create traefik-network
+
 docker compose up -d
 #wait 2-3 mins to generate acme certificates
 
