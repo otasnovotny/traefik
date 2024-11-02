cfc28d1c622197fd3caf08567ff2514e40624965
Ota Novotný <otas.novotny@gmail.com>
2024-11-02 19:11:29 +0000

    tlsChallegne instead of dnsChallenge (route53), admin credentials from .env, structural changes

--- init.sh
+++ init.sh
@@ -10,8 +10,11 @@ sudo chmod 600 acme.json
 # create network
 docker network create traefik-network
 
-docker compose up -d
-#wait 2-3 mins to generate acme certificates
-
 # generate traefik user/hashed-password
 echo $(htpasswd -nbB USER "PASS") | sed -e s/\\$/\\$\\$/g
+
+# create .env - and sev values!
+cp .env.example .env
+
+docker compose up -d
+#wait 2-3 mins to generate acme certificates
